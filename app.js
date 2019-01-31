5adb0e2c8af6b09a015d51bc53ac8fa4f6282c30
mehmetemineker <mehmetemineker92@gmail.com>
2019-01-31 23:17:00 +0300

    moving possible development

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const constants = {
     tileOddClassName: "tile-odd",
     chessPieceClassName: "chess-piece",
     displayNoneClassName: "display-none",
+    movingPossibleClassName: "moving-possible",
+    pointClassName: "point",
     loadingDummyTimeOut: 750,
     pawn: "pawn",
     rook: "rook",
@@ -21,8 +23,8 @@ const constants = {
     initialBoardWithChessPieces: [
         ["rook black", "knight black", "bishop black", "queen black", "king black", "bishop black", "knight black", "rook black"],
         ["pawn black", "pawn black", "pawn black", "pawn black", "pawn black", "pawn black", "pawn black", "pawn black"],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "knight white", "", "", ""],
+        ["", "", "bishop white", "", "", "", "", ""],
         ["", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", ""],
         ["pawn white", "pawn white", "pawn white", "pawn white", "pawn white", "pawn white", "pawn white", "pawn white"],
@@ -52,6 +54,10 @@ function boardCreate() {
 
         for (let j = 1; j <= 8; j++) {
             let tile = document.createElement("div");
+            let point = document.createElement("div");
+
+            point.className = "point";
+
             tile.classList.add(constants.tileClassName);
             tile.dataset.name = constants.letterArray[j - 1] + (9 - i);
 
@@ -67,6 +73,7 @@ function boardCreate() {
                 tile.classList.add(constants.tileOddClassName);
             }
 
+            tile.appendChild(point);
             row.appendChild(tile);
         }
     }
@@ -84,37 +91,118 @@ function chessPiecesInit() {
                 continue;
             }
 
-            let tile = document.querySelector("[data-row-index='" + indexRow + "'][data-column-index='" + indexColumn + "']");
+            let tile = document.querySelector("." + constants.tileClassName + "[data-row-index='" + indexRow + "'][data-column-index='" + indexColumn + "']");
             let piece = document.createElement("div");
             piece.className = constants.chessPieceClassName + " " + arrayItem;
 
             let splitItem = arrayItem.split(" ");
 
-            piece.dataset.type = splitItem[0];
-            piece.dataset.color = splitItem[1];
+            piece.dataset.features = JSON.stringify({
+                type: splitItem[0],
+                color: splitItem[1],
+                positionLetter: tile.dataset.nameLetter,
+                positionNumber: +tile.dataset.nameNumber,
+                positionRowIndex: +tile.dataset.rowIndex,
+                positionColumnIndex: +tile.dataset.columnIndex
+            });
 
-            piece.dataset.positionLetter = tile.dataset.nameLetter;
-            piece.dataset.positionNumber = tile.dataset.nameNumber;
-
-            tile.addEventListener("click", onPieceClick);
+            piece.addEventListener("click", onPieceClick);
             tile.appendChild(piece);
         }
     }
 }
 
 function onPieceClick(e) {
-    const pieceFeatures = {
-        type: e.target.dataset.type,
-        color: e.target.dataset.color,
-        positionLetter: e.target.dataset.positionLetter,
-        positionNumber: e.target.dataset.positionNumber
-    };
+    const pieceFeatures = JSON.parse(e.target.dataset.features);
 
     if (pieceFeatures.type === constants.knight) {
         knightMovingAbility(pieceFeatures);
+    } else if (pieceFeatures.type === constants.bishop) {
+        bishopMovingAbility(pieceFeatures);
     }
 }
 
 function knightMovingAbility(pieceFeatures) {
-    console.log(pieceFeatures);
+    const movesArray = [
+        [0, 1, 0, 1, 0],
+        [1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0]
+    ];
+
+    for (let indexRow = 0; indexRow < 5; indexRow++) {
+        for (let indexColumn = 0; indexColumn < 5; indexColumn++) {
+            const item = movesArray[indexRow][indexColumn];
+
+            if (item === 0) {
+                continue;
+            }
+
+            let startRowIndex = indexRow + pieceFeatures.positionRowIndex - 2;
+            let startColumnIndex = indexColumn + pieceFeatures.positionColumnIndex - 2;
+            let tile = document.querySelector("." + constants.tileClassName + "[data-row-index='" + startRowIndex + "'][data-column-index='" + startColumnIndex + "']");
+
+            if (tile) {
+                let child = tile.childNodes[1];
+                if (!(child && child.classList.contains(pieceFeatures.color))) {
+                    tile.classList.toggle(constants.movingPossibleClassName);
+                }
+            }
+        }
+    }
+}
+
+function bishopMovingAbility(pieceFeatures) {
+    const movesArray = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ];
+
+    for (let indexRow = 0; indexRow < 15; indexRow++) {
+        for (let indexColumn = 0; indexColumn < 15; indexColumn++) {
+            const item = movesArray[indexRow][indexColumn];
+
+            if (item === 0) {
+                continue;
+            }
+
+            let startRowIndex = indexRow + pieceFeatures.positionRowIndex - 7;
+            let startColumnIndex = indexColumn + pieceFeatures.positionColumnIndex - 7;
+            let tile = document.querySelector("." + constants.tileClassName + "[data-row-index='" + startRowIndex + "'][data-column-index='" + startColumnIndex + "']");
+
+            if (tile) {
+                let child = tile.childNodes[1];
+                if (!(child && child.classList.contains(pieceFeatures.color))) {
+                    tile.classList.toggle(constants.movingPossibleClassName);
+                }
+            }
+        }
+    }
+}
+
+function clearMovingAbility() {
+    var points = document.getElementsByClassName(constants.pointClassName);
+
+    while (points[0]) {
+        points[0].parentNode.removeChild(points[0]);
+    }
+
+    var tiles = document.getElementsByClassName("tile");
+    for (var i = 0; i < tiles.length; i++) {
+        tiles[i].classList.remove(constants.movingPossibleClassName);
+    }
 }
